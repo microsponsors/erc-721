74deff8680a4e4e02d3e59ccffc2cf571a8afe03
Lauren Garcia <lauren.garcia@gmail.com>
2019-08-20 22:54:58 -0700

    compile to /build

--- contracts/Microsponsors.sol
+++ contracts/Microsponsors.sol
@@ -1,14 +1,22 @@
 pragma solidity ^0.5.0;
 
-import "./ERC721Mintable.sol";
+import "./ERC721.sol";
 import "@0x/contracts-utils/contracts/src/Ownable.sol";
+
+
+// Copy of Deployed Registry contract ABI
+// We just use the signatures of the parts we need to interact with:
+contract DeployedRegistry {
+    mapping (address => bool) public isWhitelisted;
+}
+
 
 /**
  * Customized for Microsponsors
  * from Open Zeppelin's ERC721Metadata contract:
  * https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721Metadata.sol
  */
-contract ERC721 is ERC721Mintable, Ownable {
+contract Microsponsors is ERC721, Ownable {
 
     // Token name
     string private _name;
@@ -16,9 +24,11 @@ contract ERC721 is ERC721Mintable, Ownable {
     // Token symbol
     string private _symbol;
 
-    // Optional mapping for token URIs
-    mapping(uint256 => string) private _tokenURIs;
+    // Microsponsors Registry (whitelist)
+    DeployedRegistry public registry;
 
+    // Mapping for token URIs
+    mapping(uint256 => string) private _tokenURIs;
 
     /*
      *     bytes4(keccak256('name()')) == 0x06fdde03
@@ -57,13 +67,95 @@ contract ERC721 is ERC721Mintable, Ownable {
     }
 
     /**
-     * @dev Returns an URI for a given token ID.
-     * Throws if the token ID does not exist. May return an empty string.
-     * @param tokenId uint256 ID of the token to query
+     * @dev Update address for Microsponsors Registry contract
+     * @param newAddress where the Registry contract lives
      */
-    function tokenURI(uint256 tokenId) external view returns (string memory) {
-        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
-        return _tokenURIs[tokenId];
+    function updateRegistryAddress(address newAddress) public onlyOwner {
+        require(msg.sender == owner, 'NOT_AUTHORIZED');
+        registry = DeployedRegistry(newAddress);
+    }
+
+    /**
+     * @dev Checks Registry contract for whitelisted status
+     * @param target The address to check
+     */
+    function isWhitelisted(address target) public view returns (bool) {
+        return registry.isWhitelisted(target);
+    }
+
+    function isMinter(address account) public view returns (bool) {
+        return isWhitelisted(account);
+    }
+
+    modifier onlyMinter() {
+        require(isMinter(_msgSender()), "MinterRole: caller does not have the Minter role");
+        _;
+    }
+
+    /**
+     * @dev Function to mint tokens.
+     * @param to The address that will receive the minted token.
+     * @param tokenId The token id to mint.
+     * @return A boolean that indicates if the operation was successful.
+     */
+    function mint(address to, uint256 tokenId) public onlyMinter returns (bool) {
+        _mint(to, tokenId);
+        return true;
+    }
+
+    /**
+     * Customized for Microsponsors from:
+     * https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721MetadataMintable.sol
+     *
+     * @dev Function to mint tokens.
+     * @param to The address that will receive the minted tokens.
+     * @param tokenId The token id to mint.
+     * @param tokenURI The token URI of the minted token.
+     * @return A boolean that indicates if the operation was successful.
+     */
+    function mintWithTokenURI(address to, uint256 tokenId, string memory tokenURI) public onlyMinter returns (bool) {
+        _mint(to, tokenId);
+        _setTokenURI(tokenId, tokenURI);
+        return true;
+    }
+
+    /**
+     * @dev Function to safely mint tokens.
+     * @param to The address that will receive the minted token.
+     * @param tokenId The token id to mint.
+     * @return A boolean that indicates if the operation was successful.
+     */
+    function safeMint(address to, uint256 tokenId) public onlyMinter returns (bool) {
+        _safeMint(to, tokenId);
+        return true;
+    }
+
+    /**
+     * @dev Function to safely mint tokens.
+     * @param to The address that will receive the minted token.
+     * @param tokenId The token id to mint.
+     * @param _data bytes data to send along with a safe transfer check.
+     * @return A boolean that indicates if the operation was successful.
+     */
+    function safeMint(address to, uint256 tokenId, bytes memory _data) public onlyMinter returns (bool) {
+        _safeMint(to, tokenId, _data);
+        return true;
+    }
+
+    /**
+     * Customized for Microsponsors from
+     * https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721MetadataMintable.sol
+     *
+     * @dev Function to safely mint tokens.
+     * @param to The address that will receive the minted tokens.
+     * @param tokenId The token id to mint.
+     * @param tokenURI The token URI of the minted token.
+     * @return A boolean that indicates if the operation was successful.
+     */
+    function safeMintWithTokenURI(address to, uint256 tokenId, string memory tokenURI) public onlyMinter returns (bool) {
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, tokenURI);
+        return true;
     }
 
     /**
@@ -78,14 +170,24 @@ contract ERC721 is ERC721Mintable, Ownable {
     }
 
     /**
+     * @dev Returns an URI for a given token ID.
+     * Throws if the token ID does not exist. May return an empty string.
+     * @param tokenId uint256 ID of the token to query
+     */
+    function tokenURI(uint256 tokenId) external view returns (string memory) {
+        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+        return _tokenURIs[tokenId];
+    }
+
+    /**
      * @dev Internal function to burn a specific token.
      * Reverts if the token does not exist.
      * Deprecated, use _burn(uint256) instead.
-     * @param owner owner of the token to burn
+     * @param tokenOwner owner of the token to burn
      * @param tokenId uint256 ID of the token being burned by the msg.sender
      */
-    function _burn(address owner, uint256 tokenId) internal {
-        super._burn(owner, tokenId);
+    function _burn(address tokenOwner, uint256 tokenId) internal {
+        super._burn(tokenOwner, tokenId);
 
         // Clear metadata (if any)
         if (bytes(_tokenURIs[tokenId]).length != 0) {
