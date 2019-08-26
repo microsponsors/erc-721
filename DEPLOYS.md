09eedc5ea48c6d0fc9293644ed8e305c25e2f3ef
Lauren Garcia <lauren.garcia@gmail.com>
2019-08-26 16:29:02 -0700

    DEPLOYS.md

---
+++ DEPLOYS.md
@@ -0,0 +1,5 @@
+# DEPLOYS
+
+### 2019.08.26
+git tag: v0.1
+* [Kovan](https://kovan.etherscan.io/address/0xf47b288d1dedcb9b9f3c0a52191ebcc76f76eb57) 0xf47b288d1dedcb9b9f3c0a52191ebcc76f76eb57
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,7 +5,7 @@ module.exports = function(deployer) {
   deployer.deploy(
     Microsponsors,
     "MicrosponsorsTest", // Name
-    "MSPTest", // Symbol
+    "MSPT", // Symbol
     // Microsponsors Registry address on Kovan:
     "0xd668f99c616eb657df5c45673d6897dd29b069c9"
    );
