7ed199582a0c035dce4bf885e272aa113a64e607
Lauren Garcia <lauren.garcia@gmail.com>
2019-12-29 17:49:11 -0800

    update deploy invocation

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,14 +4,10 @@ module.exports = function(deployer) {
 
   deployer.deploy(
     Microsponsors,
-    "Microsponsors", // Name
-    "MSP", // Symbol
+    "Microsponsors Time Slots", // Name
+    "MSPT", // Symbol
     // Kovan: Microsponsors Registry v0.2 address:
     "0xcac14f367a032c14563a5ade63e33f00fe0f4c89"
    );
 
 };
-
-// Kovan: Microsponsors Registry v0.1
-// "0xd668f99c616eb657df5c45673d6897dd29b069c9"
-
