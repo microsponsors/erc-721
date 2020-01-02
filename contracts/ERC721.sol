7e88eff235848b609f647729ed501f13667da231
Lauren Garcia <lauren.garcia@gmail.com>
2020-01-02 14:53:48 -0800

    isTrader() signature

--- contracts/ERC721.sol
+++ contracts/ERC721.sol
@@ -21,7 +21,7 @@ import "./ERC165.sol";
 contract DeployedRegistry {
     function isContentIdRegisteredToCaller(string calldata contentId) public view returns(bool);
     function isMinter(address account) public view returns (bool);
-    function isTrader(address target) public view returns(bool);
+    function isTrader(address account) public view returns(bool);
     function isAuthorizedTransferFrom(address from, address to, uint256 tokenId) public view returns(bool);
     function isAuthorizedResale(address from, address to, uint256 tokenId) public view returns(bool);
 }
